03687632c93f15a077b0bd73e59011d9b5f2d43a
chi057 <zlin90228@gmail.com>
2023-05-22 15:09:49 +0800

    2023/0522 - 介面設計
    1.設計介面
    2.控制項設定名稱
    3.更改背景顏色

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace _7thMeet
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
